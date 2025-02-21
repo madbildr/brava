c0ae008fa90e0d96c9cb755fe8e15a866ad7301c
madbildr <114622367+madbildr@users.noreply.github.com>
2025-02-21 01:12:16 +0000

    Update form-scripts.js

--- form-scripts.js
+++ form-scripts.js
@@ -128,7 +128,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
     function loadGoogleMapsScript(apiKey) {
         const script = document.createElement("script");
-        script.src = `https://maps.googleapis.com/maps/api/js?key=${apiKey}&libraries=places&callback=initMap`;
+        script.src = `https://maps.googleapis.com/maps/api/js?key=${apiKey}&libraries=places`;
         script.async = true;
         script.defer = true;
         document.head.appendChild(script);
